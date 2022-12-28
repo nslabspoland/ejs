b8349011f57fc5bfb0a15457c0cb5eee506465bd
wojtekxtx <wojtekx.tx@gmail.com>
2022-12-28 12:48:22 +0100

    Update dev-env.js

--- config/dev-env.js
+++ config/dev-env.js
@@ -1,2 +1,11 @@
 export const repoaddress = "https://github.com/nslabspl/ejs";
-export const thisAppName = "Everything JS"
+export const thisAppName = "Everything JS";
+export const instanceType = "dev";
+
+/** Experimental.
+ *  May cause instability.
+ *  Use with caution
+*/
+
+export const ctxStatusOK = "200, OK";
+export const ctxStatusRedirected = "301, Redirected";
