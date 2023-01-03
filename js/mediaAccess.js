33e92c2f6393e75f96c9541705b4e60ac583476c
wojtekxtx <wojtekx.tx@gmail.com>
2023-01-03 20:24:38 +0100

    working version of player

--- js/mediaAccess.js
+++ js/mediaAccess.js
@@ -2,26 +2,28 @@
  * @author @wojtekxtx
  * @class media/access
  * @description Defines access to user media devices and controls app permissions.
- * @todo set more granular properties for each device
  * @see https://4programmers.net/Forum/JavaScript/365524-dostep_do_mikrofonu_i_co_dalej_jak_korzystac_z_webrtc
  * @version 0.0.1
  */
 function getAllAvailableUserMedia() {
   return [
+    element = document.querySelector('video'),
     navigator.mediaDevices
       .getUserMedia({
         audio: true,
         video: true,
       })
       .then(function Stream() {
-        let videoPlayer = document.querySelector('video');
-        document.getElementById('playvid_btn') = videoPlayer.play();
-        videoPlayer.height = 1280;
-        videoPlayer.width = 960;
+        dotenv.config();
+        document.getElementById('playvid_btn') = element.play();
+        element.height = 1280;
+        element.width = 960;
+        element.autoplay = process.env.V_AUTOPLAY;
       })
       .catch(function (e) {
         e = Error.prototype.message();
         console.error(e);
-      }),
+      })
+    .then(deleteElement(element))
   ];
 }
---
+++ js/std_app_fn.js
@@ -0,0 +1,5 @@
+function deleteElement(element) {
+	let el = document.getElementById(element).style.display = "none";
+}
+
+export {deleteElement}
