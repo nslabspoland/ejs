fe589ce19ac1e8c58ff6ecc07c58bfbc49840299
wojtekxtx <wojtekx.tx@gmail.com>
2023-02-18 13:34:27 +0100

    add jsdocs()

--- js/escaper.js
+++ js/escaper.js
@@ -1,5 +1,15 @@
 let str = "";
 
+/**
+ * Escapes HTML code to safe form.
+ * 
+ * @param {*} str 
+ * @returns str
+ * @author @wojtekxtx
+ * @since 0.1.1
+ * @see https://palant.info/2020/03/02/psa-jquery-is-bad-for-the-security-of-your-project/#harmful-coding-patterns
+ * @see nslabspl/ejs#2
+ */
 export default function HTMLEscaper(str) {
 	return str
 		.replace("/</g", "&lt;")
