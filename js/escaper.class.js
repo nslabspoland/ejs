23483ebfb10aae3cc82b8fc4bf2a099f13fc82dc
wojtekxtx <wojtekx.tx@gmail.com>
2023-02-21 15:28:44 +0100

    commentary

--- js/escaper.class.js
+++ js/escaper.class.js
@@ -1,16 +1,18 @@
 export default class Escaper {
 
 	str = "";
+	par = Math.floor(Math.random(0, 255));
 
 	constructor(
-		str = this.str
+		str = this.str,
+		par = this.par
 	)
 
 	fromHtml() {
 		return this.str
 			.replace("/</g", "&lt;")
 			.replace("/>/g", "&gt;")
-			.replace("//g", "&quot;")
+			.replace("/ /g", "&quot;") // Whitespaces
 			.replace("rf/", "ln")
 			.replace("/&/g", "&amp;");
 	}
@@ -18,6 +20,8 @@ export default class Escaper {
 	systemPaths() {
 		return this.str
 			.replace("///", "/")
-			.replace("//", "/");
+			.replace("//", "/")
+			.replace("\\", "\\")
+			.replace("\eol", "\n");
 	}
 }
