9279ce82b29242bda5bb0ac41d11610c4dc6ff19
wojtekxtx <wojtekx.tx@gmail.com>
2023-03-14 10:10:16 +0100

    add basic project-wide config

--- config/winstonconf.js
+++ config/winstonconf.js
@@ -1,2 +1,20 @@
-import winstonLogger from 'winston';
-import winston from 'winston/lib/winston/config';
+import winstonLogger from "winston";
+import { winstonLoggerFilePath } from "../components/constants";
+
+// Create logger instance with custom logging transports
+winstonLogger.createLogger({
+	level: 'info',
+	transports: [
+		new winstonLogger.transports.Console({
+			format: winstonLogger.format.combine(
+				winstonLogger.format.colorize(),
+				winstonLogger.format.json()
+			)
+		}),
+		new winstonLogger.transports.File(winstonLoggerFilePath),
+	],
+});
+
+// Add the most used transport
+winstonLogger.add(winstonLogger.transports.File(winstonLoggerFilePath));
+
